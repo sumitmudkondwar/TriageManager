e42dfa8b457adfa1646563f713c460576c0bec3d
hkharan <hkrish@microsoft.com>
2016-11-07 17:38:18 +0530

    Report

--- TriageManager/TDP/Report.aspx
+++ TriageManager/TDP/Report.aspx
@@ -1,29 +1,93 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/TDP/NestedMasterPage1.master" AutoEventWireup="true" CodeBehind="Report.aspx.cs" Inherits="TriageManager.TDP.Report" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="TDPContent" runat="server">
 <asp:Panel ID="pnlReport" runat="server">
+<style>
+.GridViewClass td,th
+{
+    padding-left:10px;    
+}
+</style>
+<br />
+<br />
+    <h3>Consolidated View</h3>
+<br />
+    <asp:GridView ID="grdConsolidate" runat="server" CellPadding="4" DataKeyNames="EngineerName" ForeColor="Black" GridLines="Vertical" CssClass="GridViewClass" OnDataBound="grdConsolidate_DataBound" OnRowDataBound="grdConsolidate_RowDataBound" BackColor="White" BorderColor="#DEDFDE" BorderStyle="None" BorderWidth="1px">
+        <AlternatingRowStyle BackColor="White" />
+        <FooterStyle BackColor="#CCCC99" />
+        <HeaderStyle BackColor="#6B696B" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#F7F7DE" ForeColor="Black" HorizontalAlign="Right" />
+        <RowStyle BackColor="#F7F7DE" Wrap="False" />
+        <SelectedRowStyle BackColor="#CE5D5A" Font-Bold="True" ForeColor="White" />
+        <SortedAscendingCellStyle BackColor="#FBFBF2" />
+        <SortedAscendingHeaderStyle BackColor="#848384" />
+        <SortedDescendingCellStyle BackColor="#EAEAD3" />
+        <SortedDescendingHeaderStyle BackColor="#575357" />
+    </asp:GridView>
 <br />
 <br />
     <h3>Initial Engineer Assessment</h3>
 <br />
-    <asp:GridView ID="grdEngAssess" runat="server">
+    <asp:GridView ID="grdEngAssess" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" CssClass="GridViewClass">
+        <AlternatingRowStyle BackColor="White" />
+        <EditRowStyle BackColor="#2461BF" />
+        <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#EFF3FB" />
+        <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#F5F7FB" />
+        <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+        <SortedDescendingCellStyle BackColor="#E9EBEF" />
+        <SortedDescendingHeaderStyle BackColor="#4870BE" />
     </asp:GridView>
 <br />
 <br />
     <h3>Initial TA Assessment</h3>
 <br />
-    <asp:GridView ID="grdTAAssess" runat="server">
+    <asp:GridView ID="grdTAAssess" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" CssClass="GridViewClass">
+        <AlternatingRowStyle BackColor="White" ForeColor="#284775" />
+        <EditRowStyle BackColor="#999999" />
+        <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+        <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+        <SortedAscendingCellStyle BackColor="#E9E7E2" />
+        <SortedAscendingHeaderStyle BackColor="#506C8C" />
+        <SortedDescendingCellStyle BackColor="#FFFDF8" />
+        <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
     </asp:GridView>
 <br />
 <br />
     <h3>TA Status Report</h3>
 <br />
-    <asp:GridView ID="grdTA" runat="server">
+    <asp:GridView ID="grdTA" runat="server" BackColor="White" BorderColor="#DEDFDE" BorderStyle="None" CssClass="GridViewClass" BorderWidth="1px" CellPadding="4" ForeColor="Black" GridLines="Vertical">
+        <AlternatingRowStyle BackColor="White" />
+        <FooterStyle BackColor="#CCCC99" />
+        <HeaderStyle BackColor="#6B696B" Font-Bold="True" ForeColor="White" />
+        <PagerStyle BackColor="#F7F7DE" ForeColor="Black" HorizontalAlign="Right" />
+        <RowStyle BackColor="#F7F7DE" />
+        <SelectedRowStyle BackColor="#CE5D5A" Font-Bold="True" ForeColor="White" />
+        <SortedAscendingCellStyle BackColor="#FBFBF2" />
+        <SortedAscendingHeaderStyle BackColor="#848384" />
+        <SortedDescendingCellStyle BackColor="#EAEAD3" />
+        <SortedDescendingHeaderStyle BackColor="#575357" />
     </asp:GridView>
 <br />
 <br />
-    <h3>Engineer Status Report</h3>
+    <h3>Current Status of Tasks</h3>
 <br />
-    <asp:GridView ID="grdEngineer" runat="server">
+    <asp:GridView ID="grdEngineer" runat="server" BackColor="LightGoldenrodYellow" BorderColor="Tan" BorderWidth="1px" CellPadding="2" ForeColor="Black" GridLines="None" OnRowDataBound="grdEngineer_RowDataBound"  CssClass="GridViewClass">
+        <AlternatingRowStyle BackColor="PaleGoldenrod" />
+        <FooterStyle BackColor="Tan" />
+        <HeaderStyle BackColor="Tan" Font-Bold="True" />
+        <PagerStyle BackColor="PaleGoldenrod" ForeColor="DarkSlateBlue" HorizontalAlign="Center" />
+        <RowStyle Wrap="False" />
+        <SelectedRowStyle BackColor="DarkSlateBlue" ForeColor="GhostWhite" />
+        <SortedAscendingCellStyle BackColor="#FAFAE7" />
+        <SortedAscendingHeaderStyle BackColor="#DAC09E" />
+        <SortedDescendingCellStyle BackColor="#E1DB9C" />
+        <SortedDescendingHeaderStyle BackColor="#C2A47B" />
     </asp:GridView>
 </asp:Panel>
 <br />
--- TriageManager/TDP/Report.aspx.cs
+++ TriageManager/TDP/Report.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -14,6 +15,8 @@ namespace TriageManager.TDP
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            { 
             DataSet ds = null;
             DataSet ds1 = null;
             DataSet ds2 = null;
@@ -37,15 +40,15 @@ namespace TriageManager.TDP
             sqldda = new SqlDataAdapter(cmd);
             sqldda.Fill(ds3);
 
-            //if (ds3.Tables[0].Rows.Count == 0 || ds3.Tables[0].Rows[0][0].ToString() == "Support Engineer")
-            //{
-            //    sqlConnection.Close();
-            //    pnlReport.Visible = false;
-            //    lblComment.Text = "Only TA/Manager can assess Reports of the engineers";
-            //    return;
-            //}
+                if (ds3.Tables[0].Rows.Count == 0 || ds3.Tables[0].Rows[0][0].ToString() == "Support Engineer")
+                {
+                    sqlConnection.Close();
+                    pnlReport.Visible = false;
+                    lblComment.Text = "Only TA/Manager can assess Reports of the engineers";
+                    return;
+                }
 
-            pnlReport.Visible = true;
+                pnlReport.Visible = true;
 
 
             cmd.CommandText = "SELECT left([firstname],10) EngineerName,[Availability / Performance],[VNET / Hybrid],[ASE],[Mobile Apps]," +
@@ -55,21 +58,42 @@ namespace TriageManager.TDP
             ds4 = new DataSet();
             sqldda = new SqlDataAdapter(cmd);
             sqldda.Fill(ds4);
+                
+                (ds4.Tables[0]).SetColumnsOrder("EngineerName", "Availability / Performance", "Deployment", "VNET / Hybrid", "ASE", "WebJobs / Functions", 
+                    "Swap / Slots", "Mobile Apps", "Easy Authentication", "AutoScale / Alerts", "BYOD / App Service Certificate", 
+                    "Powershell / ARM APIs", "Azure App Service on Linux", "OSS", "LastUpdate");
 
-            grdEngAssess.DataSource = ds4;
+                DataTable dtCloned = ds4.Tables[0].Clone();
+                foreach (DataColumn col in dtCloned.Columns)
+                    col.DataType = typeof(string); 
+
+                foreach (DataRow row in ds4.Tables[0].Rows)
+                {
+                    dtCloned.ImportRow(row);
+                }
+
+                grdEngAssess.DataSource = ds4;
             grdEngAssess.DataBind();
 
-            cmd.CommandText = "select rep.*, left([Users].[firstName],10) AssessedBy from (SELECT left([firstname],10) EngineerName,[Availability / Performance],[VNET / Hybrid],[ASE],[Mobile Apps]," +
+
+                cmd.CommandText = "select rep.*, left([Users].[firstName],10) AssessedBy from (SELECT left([firstname],10) EngineerName,[Availability / Performance],[VNET / Hybrid],[ASE],[Mobile Apps]," +
                 "[WebJobs / Functions],[Azure App Service on Linux],[Deployment],[Easy Authentication],[AutoScale / Alerts],[Swap / Slots]," +
                 "[BYOD / App Service Certificate],[Powershell / ARM APIs],[OSS],[Other Configuration],[Stress Testing]" +
                 ",[TAAssessmentBy],convert(varchar, TRY_PARSE([TAAssessmentDate] as date)) LastUpdate FROM[dbo].[Assessment], [Users] where [TAAssessment] = 'Yes'  and [Assessment].Engineer = [Users].emailID) rep, [Users] " +
-                "where rep.[TAAssessmentBy] = [Users].emailID ";
+                "where rep.[TAAssessmentBy] = [Users].emailID order by EngineerName";
             ds5 = new DataSet();
             sqldda = new SqlDataAdapter(cmd);
             sqldda.Fill(ds5);
             ds5.Tables[0].Columns.RemoveAt(16);
 
-            grdTAAssess.DataSource = ds5;
+
+                ds5.Tables[0].DefaultView.Sort = "EngineerName asc";
+
+                (ds5.Tables[0]).SetColumnsOrder("EngineerName", "Availability / Performance", "Deployment", "VNET / Hybrid", "ASE", "WebJobs / Functions",
+                    "Swap / Slots", "Mobile Apps", "Easy Authentication", "AutoScale / Alerts", "BYOD / App Service Certificate",
+                    "Powershell / ARM APIs", "Other Configuration", "Stress Testing", "Azure App Service on Linux", "OSS", "AssessedBy", "LastUpdate");
+
+                grdTAAssess.DataSource = ds5;
             grdTAAssess.DataBind();
 
             cmd.CommandText = "SELECT emailID, left(firstName,10) EngineerName from Users where [Designation] = 'Support Engineer'";
@@ -82,7 +106,7 @@ namespace TriageManager.TDP
             sqldda = new SqlDataAdapter(cmd);
             sqldda.Fill(ds1);
 
-            cmd.CommandText = "select task, engineer, engineerstatus, tastatus from TDPtasks";
+            cmd.CommandText = "select task, engineer, concat(left(isnull(engineerstatus,'NULL'),4), ' | ', left(isnull(tastatus,'NULL'),4)), convert(varchar, TRY_PARSE([ETCompletion] as date)) ETA from TDPtasks";
             ds2 = new DataSet();
             sqldda = new SqlDataAdapter(cmd);
             sqldda.Fill(ds2);
@@ -106,39 +130,249 @@ namespace TriageManager.TDP
                 dt.Rows[count - 1][1] = row[1].ToString();
             }
 
-            dt.DefaultView.Sort = "EngineerName asc";
             DataTable dt1 = dt.Copy();
+                DataTable eta = dt.Copy();
+                dt.DefaultView.Sort = "EngineerName asc";
+                dt1.DefaultView.Sort = "EngineerName asc";
+                eta.DefaultView.Sort = "EngineerName asc";
 
-            foreach (DataRow row in ds2.Tables[0].Rows)
+                foreach (DataRow row in ds2.Tables[0].Rows)
             {
                 string select = "Engineer ='" + row[1].ToString() + "'";
                 DataRow[] result = dt.Select(select.ToString());
 
-                int SelectedIndex = dt.Rows.IndexOf(result[0]);
-                dt.Rows[SelectedIndex][row[0].ToString()] = row[3].ToString();
+                    int SelectedIndex = dt.Rows.IndexOf(result[0]);
+                dt.Rows[SelectedIndex][row[0].ToString()] = row[2].ToString();
+                    dt1.Rows[SelectedIndex][row[0].ToString()] = row[2].ToString();
+                    eta.Rows[SelectedIndex][row[0].ToString()] = row[3].ToString();
 
-            }
+                }
 
             dt.Columns.Remove("Engineer");
 
-            grdTA.DataSource = dt;
-            grdTA.DataBind();
+                dt.SetColumnsOrder("EngineerName", "Availability / Performance", "Deployment", "VNET / Hybrid", "ASE", "WebJobs / Functions",
+                    "Swap / Slots", "Mobile Apps", "Easy Authentication", "AutoScale / Alerts", "BYOD / App Service Certificate",
+                    "Powershell / ARM APIs", "Other Configuration", "Stress Testing", "Azure App Service on Linux", "OSS");
 
-            foreach (DataRow row in ds2.Tables[0].Rows)
-            {
-                string select1 = "Engineer ='" + row[1].ToString() + "'";
-                DataRow[] result = dt1.Select(select1.ToString());
+            //    grdTA.DataSource = dt;
+            //grdTA.DataBind();
 
-                int SelectedIndex = dt1.Rows.IndexOf(result[0]);
-                dt1.Rows[SelectedIndex][row[0].ToString()] = row[2].ToString();
+            //foreach (DataRow row in ds2.Tables[0].Rows)
+            //{
+            //    string select1 = "Engineer ='" + row[1].ToString() + "'";
+            //    DataRow[] result1 = dt1.Select(select1.ToString());
 
-            }
+            //    int SelectedIndex = dt1.Rows.IndexOf(result[0]);
+            //    dt1.Rows[SelectedIndex][row[0].ToString()] = row[2].ToString();
+
+            //}
 
             dt1.Columns.Remove("Engineer");
 
-            dt1.DefaultView.Sort = "EngineerName asc";
-            grdEngineer.DataSource = dt1;
+                dt1.SetColumnsOrder("EngineerName", "Availability / Performance", "Deployment", "VNET / Hybrid", "ASE", "WebJobs / Functions",
+                    "Swap / Slots", "Mobile Apps", "Easy Authentication", "AutoScale / Alerts", "BYOD / App Service Certificate",
+                    "Powershell / ARM APIs", "Other Configuration", "Stress Testing", "Azure App Service on Linux", "OSS");
+
+                grdEngineer.DataSource = dt1;
             grdEngineer.DataBind();
+
+
+                DataTable dt2 = dt1.Copy();
+                DataTable dt3 = dtCloned.Copy();
+                foreach(DataRow row in dt2.Rows)
+                {
+                    dt3.ImportRow(row);
+                }
+                foreach (DataRow row1 in eta.Rows)
+                {
+                    dt3.ImportRow(row1);
+                }
+
+                dt3.Columns.Remove("LastUpdate");
+                dt3.DefaultView.Sort = "EngineerName asc";
+
+                //grdConsolidate.DataSource = dt3;
+                //grdConsolidate.DataBind();
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+       }
+
+        protected void grdEngineer_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                foreach (TableCell cell in e.Row.Cells)
+                {
+                    //TableCell cell = e.Row.Cells[1];
+                    string text = cell.Text.ToString();
+
+                    if (text.Contains("Comp | Comp"))
+                    {
+                        cell.BackColor = Color.LawnGreen;
+                    }
+                    else if (text.Contains("Comp |"))
+                    {
+                        cell.BackColor = Color.LightGreen;
+                    }
+                    else if (text.Contains("InPr |"))
+                    {
+                        cell.BackColor = Color.Cyan;
+                    }
+                }
+            }
+        }
+
+        protected void grdConsolidate_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            try { 
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                foreach (TableCell cell in e.Row.Cells)
+                {
+                    //TableCell cell = e.Row.Cells[1];
+                    string text = cell.Text.ToString();
+
+                    if (text.Contains("Comp | Comp"))
+                    {
+                        cell.BackColor = Color.LawnGreen;
+                    }
+                    else if (text.Contains("Comp |"))
+                    {
+                        cell.BackColor = Color.LightGreen;
+                    }
+                    else if (text.Contains("InPr |"))
+                    {
+                        cell.BackColor = Color.Cyan;
+                        }
+                        //else if (text.StartsWith("2016") || text.StartsWith("2017"))
+                        //{
+                        //    string a = Convert.ToDateTime(text).ToString();
+                        //    if (DateTime.Now > Convert.ToDateTime(text))
+                        //    {
+                        //        cell.BackColor = Color.Red;
+                        //    }
+                        //    else if (DateTime.Now.AddDays(7) > Convert.ToDateTime(text))
+                        //    {
+                        //        cell.BackColor = Color.Orange;
+                        //    }
+                        //}
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void CombineColumnCells(GridViewRow currentRow, int colIndex, string fieldName)
+        {
+            try
+            {
+                TableCell currentCell = currentRow.Cells[colIndex];
+
+                if (currentCell.Visible)
+                {
+                    Object currentValue = grdConsolidate.DataKeys[currentRow.RowIndex].Values[fieldName];
+
+                    for (int nextRowIndex = currentRow.RowIndex + 1; nextRowIndex < grdConsolidate.Rows.Count; nextRowIndex++)
+                    {
+                        Object nextValue = grdConsolidate.DataKeys[nextRowIndex].Values[fieldName];
+
+                        if (nextValue.ToString() == currentValue.ToString())
+                        {
+                            GridViewRow nextRow = grdConsolidate.Rows[nextRowIndex];
+                            TableCell nextCell = nextRow.Cells[colIndex];
+                            currentCell.RowSpan = Math.Max(1, currentCell.RowSpan) + 1;
+                            nextCell.Visible = false;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        protected void grdConsolidate_DataBound(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int currentRowIndex = 0; currentRowIndex < grdConsolidate.Rows.Count; currentRowIndex++)
+                {
+                    GridViewRow currentRow = grdConsolidate.Rows[currentRowIndex];
+                    CombineColumnCells(currentRow, 0, "EngineerName");
+
+                    int cellCount = currentRow.Cells.Count;
+                    int i = 0;
+
+                    foreach(TableCell cell in currentRow.Cells)
+                    {
+                        if (i == cellCount)
+                            i = 0;
+                        if ((cell.Text.StartsWith("2016") || cell.Text.StartsWith("2017")) && (!grdConsolidate.Rows[currentRowIndex - 1].Cells[i].Text.Contains("Comp")))
+                        {
+                            if (DateTime.Now > Convert.ToDateTime(cell.Text))
+                            {
+                                cell.BackColor = Color.Red;
+                            }
+                            else if (DateTime.Now.AddDays(7) > Convert.ToDateTime(cell.Text))
+                            {
+                                cell.BackColor = Color.Orange;
+                            }
+                        }
+                        else if ((cell.Text.StartsWith("2016") || cell.Text.StartsWith("2017")))
+                        {
+
+                        }
+
+                        i = i + 1;
+                    }
+                }
+
+                //foreach (DataGridColumn col in grdConsolidate.Columns)
+                //{
+                //    this.grdConsolidate.Columns.si
+                //    col.s
+                //    DataGridViewColumn.AutoSizeMode
+                //}
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+
+
+
+    public static class DataTableExtensions
+    {
+        public static void SetColumnsOrder(this DataTable table, params String[] columnNames)
+        {
+            try
+            {
+                int columnIndex = 0;
+                foreach (var columnName in columnNames)
+                {
+                    table.Columns[columnName].SetOrdinal(columnIndex);
+                    columnIndex++;
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
